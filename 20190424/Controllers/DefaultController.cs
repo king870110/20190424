438323909975cfb1b8fc40a44e27f4cbd36a0899
多鴻 金 <king870110@gmail.com>
2019-05-02 13:23:22 +0800

    21

---
+++ 20190424/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20190424.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Label = "asdasddasasfdsfw";
+            return View();
+        }
+    }
+}
